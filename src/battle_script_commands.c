349799f6f1b0da6ad3f7bf82ad23168abd2e6a97
Plish Plash <plishhplashh@gmail.com>
2023-06-24 12:55:18 -0700

    Ask to continue learning instead of stop learning move

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -7558,7 +7558,7 @@ static void Cmd_yesnoboxstoplearningmove(void)
         {
             PlaySE(SE_SELECT);
 
-            if (gBattleCommunication[1] != 0)
+            if (gBattleCommunication[1] == 0)
                 gBattlescriptCurrInstr = cmd->noInstr;
             else
                 gBattlescriptCurrInstr = cmd->nextInstr;
@@ -7568,7 +7568,7 @@ static void Cmd_yesnoboxstoplearningmove(void)
         else if (JOY_NEW(B_BUTTON))
         {
             PlaySE(SE_SELECT);
-            gBattlescriptCurrInstr = cmd->noInstr;
+            gBattlescriptCurrInstr = cmd->nextInstr;
             HandleBattleWindow(YESNOBOX_X_Y, WINDOW_CLEAR);
         }
         break;
